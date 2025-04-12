79d9ad9eccc346300d938c90641f17e04867968e
Marqueane Souza <seu-email@exemplo.com>
2025-04-12 20:12:10 -0300

    Criado models aluno

--- alunos/models.py
+++ alunos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    telefome = models.CharField(max_length=20)
+    #cpf = models.CharField(max_length=14)
+    cpf = models.CharField(max_length=14, unique=True) #restrição para garantir cpf com valores únicos. Dado por outra IA
+    email = models.EmailField()
+    universidade = models.CharField(max_length=100)
+    curso = models.CharField(max_length=50)
+    data_admissao = models.DateField()
+    previsao_formatura = models.DateField()
+    data_exclusao = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+    
