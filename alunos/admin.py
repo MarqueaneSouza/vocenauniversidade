c46f5b8d47b474971f50ca10e73c0b63c9cc62b0
Marqueane Souza <seu-email@exemplo.com>
2025-04-17 19:31:09 -0300

    Atualiza AlunoAdmin e cria template alunos/painel.html

--- alunos/admin.py
+++ alunos/admin.py
@@ -4,12 +4,21 @@ from .models import Acompanhamento, Aluno
 
 @admin.register(Aluno)
 class AlunoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cpf', 'email', 'curso', 'universidade', 'previsao_formatura') # o que aparece na lista
-    search_fields = ('nome', 'cpf', 'email', 'curso', 'universidade') # barra de busca
-    list_filter = ('universidade', 'curso', 'previsao_formatura') # filtros do lado direito
+    list_display = (
+        'nome',
+        'telefone',
+        'cpf',
+        'email',
+        'universidade',
+        'curso',
+        'data_admissao',
+        'previsao_formatura',
+    )
 
 @admin.register(Acompanhamento)
 class AcompanhamentoAdmin(admin.ModelAdmin):
-    list_display = ('aluno', 'ano', 'periodo')
+    list_display = ('aluno', 'ano', 'periodo', 'rendimento')
     search_fields = ('aluno_nome',)
     list_filter = ('ano', 'periodo')
+
+    #parei   4. Criar o template: templates/aluno/painel.html
--- alunos/views.py
+++ alunos/views.py
@@ -4,5 +4,5 @@ from alunos.models import Aluno, Acompanhamento
 
 def painel_acompanhamento(request):
     acompanhamentos = Acompanhamento.objects.select_related('aluno').all()
-    return render(request, 'aluno/painel.html', {'acompanhamentos': acompanhamentos})
+    return render(request, 'alunos/painel.html', {'acompanhamentos': acompanhamentos})
     
