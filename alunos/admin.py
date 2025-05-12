69968e3a89e3c2ce7a1adc936006a331e18a49a8
Marqueane Souza <seu-email@exemplo.com>
2025-05-12 20:33:01 -0300

    Adiciona controle de status ativo/inativo com justificativa para alunos

--- alunos/admin.py
+++ alunos/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Acompanhamento, Aluno
 
 @admin.register(Aluno)
@@ -13,8 +12,26 @@ class AlunoAdmin(admin.ModelAdmin):
         'curso',
         'admissao_formatada',
         'formatura_formatada',
+        'status',
     )
+    list_filter = ('status',)
 
+
+    @admin.display(description="Admissão")
+    def admissao_formatada(self, obj):
+        if obj.mes_admissao and obj.ano_admissao:
+            return f"{obj.mes_admissao}/{obj.ano_admissao}"
+        elif obj.ano_admissao:
+            return f"{obj.ano_admissao}"
+        return "-"
+
+    @admin.display(description="Formatura")
+    def formatura_formatada(self, obj):
+        if obj.mes_formatura and obj.ano_formatura:
+            return f"{obj.mes_formatura}/{obj.ano_formatura}"
+        elif obj.ano_formatura:
+            return f"{obj.ano_formatura}"
+        return "-"
 
 
 @admin.register(Acompanhamento)
@@ -22,3 +39,4 @@ class AcompanhamentoAdmin(admin.ModelAdmin):
     list_display = ('aluno', 'ano', 'periodo', 'rendimento')
     search_fields = ('aluno_nome',)
     list_filter = ('ano', 'periodo')
+
--- alunos/models.py
+++ alunos/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 
+
+MESES = [
+    ('01', 'Janeiro'),
+    ('02', 'Fevereiro'),
+    ('03', 'Março'),
+    ('04', 'Abril'),
+    ('05', 'Maio'),
+    ('06', 'Junho'),
+    ('07', 'Julho'),
+    ('08', 'Agosto'),
+    ('09', 'Setembro'),
+    ('10', 'Outubro'),
+    ('11', 'Novembro'),
+    ('12', 'Dezembro'),
+]
+
+ANOS = [(str(ano), str(ano)) for ano in range(2000, 2035)]
+
+STATUS_CHOICES = [
+    ('ativo', 'Ativo'),
+    ('inativo', 'Inativo',)
+]
+
 class Aluno(models.Model):
     nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
@@ -7,28 +30,58 @@ class Aluno(models.Model):
     email = models.EmailField()
     universidade = models.CharField(max_length=100)
     curso = models.CharField(max_length=50)
-    semestre_admissão = models.IntegerField(choices=[(1, '1º semestre'), (2, '2º semestre')])
-    ano_admissao = models.IntegerField()
-    semestre_formatura = models.IntegerField(choices=[(1, '1º semestre'), (2, '2º semestre')])
-    ano_formatura = models.IntegerField()
+
+    mes_admissao = models.CharField(max_length=2, choices=MESES, blank=True, null=True)
+    ano_admissao = models.IntegerField(blank=True, null=True)
+    mes_formatura = models.CharField(max_length=2, choices=MESES, blank=True, null=True)
+    ano_formatura = models.IntegerField(blank=True, null=True)
+
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='ativo'
+    )
+
+    justificativa_exclusão = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Informe o motivo da exclusão"
+    )
+    
+    def clean(self):
+        from django.core.exceptions import ValidationError
+
+        if self.status == 'inativo' and not self.justificativa_exclusão:
+            if self.status == 'inativo' and not self.justificativa_inatividade:
+             raise ValidationError({'justificativa_inatividade': 'Obrigatório informar a justificativa se o aluno estiver inativo.'})
+    
+    def admissao_formatada(self):
+        if self.mes_admissao and self.ano_admissao:
+            return f"{self.mes_admissao}/{self.ano_admissao}"
+        elif self.ano_admissao:
+            return f"{self.ano_admissao}"
+        return "-"
+
+    def formatura_formatada(self):
+        if self.mes_formatura and self.ano_formatura:
+            return f"{self.mes_formatura}/{self.ano_formatura}"
+        elif self.ano_formatura:
+            return f"{self.ano_formatura}"
+        return "-"
+
     data_exclusao = models.DateField(null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        self.nome = self.nome.upper()
-        self.nome = self.nome.upper()
-        self.curso = self.curso.upper()
+        if self.nome:
+            self.nome = self.nome.upper()
+        if self.universidade:   
+            self.universidade = self.universidade.upper()
+        if self.curso:
+            self.curso = self.curso.upper()
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.nome
-    
-    @property
-    def admissao_formatada(self):
-        return f"{self.semestre_admissao}º Semestre de {self.ano_admissao}"
-
-    @property
-    def formatura_formatada(self):
-        return f"{self.semestre_formatura}º Semestre de {self.ano_formatura}"
 
 class Acompanhamento(models.Model):
     aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name='acompanhamentos')
